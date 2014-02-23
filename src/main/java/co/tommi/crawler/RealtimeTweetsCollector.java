d5d3ecede3e3a24953d55e748f7ee65e22b25fe0
Tommi <me@tommi.co>
2014-02-23 12:58:16 +0800

    Rename App.java to RealtimeTweetsCollector.java

--- src/main/java/co/tommi/crawler/RealtimeTweetsCollector.java
+++ src/main/java/co/tommi/crawler/RealtimeTweetsCollector.java
@@ -20,7 +20,7 @@ import com.twitter.hbc.core.processor.StringDelimitedProcessor;
 import com.twitter.hbc.httpclient.auth.Authentication;
 import com.twitter.hbc.httpclient.auth.OAuth1;
 
-public class App {
+public class RealtimeTweetsCollector {
 	static AmazonKinesisClient kinesisClient;
 	static Client hosebirdClient;
 
