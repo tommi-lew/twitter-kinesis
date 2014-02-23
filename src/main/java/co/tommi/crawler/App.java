beda7d7ebd0ad595ddb00608001b09bf186122e4
Tommi <me@tommi.co>
2014-02-23 12:56:35 +0800

    Refactor App.java

--- src/main/java/co/tommi/crawler/App.java
+++ src/main/java/co/tommi/crawler/App.java
@@ -8,6 +8,7 @@ import java.util.concurrent.LinkedBlockingQueue;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.kinesis.AmazonKinesisClient;
 import com.amazonaws.services.kinesis.model.PutRecordRequest;
+import com.amazonaws.services.kinesis.model.PutRecordResult;
 import com.google.common.collect.Lists;
 import com.twitter.hbc.ClientBuilder;
 import com.twitter.hbc.core.Client;
@@ -15,17 +16,42 @@ import com.twitter.hbc.core.Constants;
 import com.twitter.hbc.core.Hosts;
 import com.twitter.hbc.core.HttpHosts;
 import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
-import com.twitter.hbc.core.event.Event;
 import com.twitter.hbc.core.processor.StringDelimitedProcessor;
 import com.twitter.hbc.httpclient.auth.Authentication;
 import com.twitter.hbc.httpclient.auth.OAuth1;
 
 public class App {
-	public static void main(String[] args) {
-		/** Set up your blocking queues: Be sure to size these properly based on expected TPS of your stream */
-		BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(100000);
-		BlockingQueue<Event> eventQueue = new LinkedBlockingQueue<Event>(1000);
+	static AmazonKinesisClient kinesisClient;
+	static Client hosebirdClient;
 
+	/** Set up your blocking queues: Be sure to size these properly based on expected TPS of your stream */
+	static BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(100000);
+
+	public static void main(String[] args) {
+		setupKinesisClient();
+		setupHosebirdClient();
+		hosebirdClient.connect();
+
+		while (!hosebirdClient.isDone()) {
+			try {
+				String tweetText = msgQueue.take();
+
+				// Add Data to a Stream
+				PutRecordRequest putRecordRequest = new PutRecordRequest();
+				putRecordRequest.setStreamName(Helper.properties().getProperty("kinesisStreamName"));
+				putRecordRequest.setData(ByteBuffer.wrap(tweetText.getBytes()));
+				putRecordRequest.setPartitionKey(String.format("partitionKey-%s", "tweets"));
+				PutRecordResult putRecordResult = kinesisClient.putRecord(putRecordRequest);
+
+				System.out.println(String.format("Seq No: %s - %s", putRecordResult.getSequenceNumber(), tweetText));
+
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+    }
+
+	public static void setupHosebirdClient() {
         /** Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth) */
 		Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
 		StatusesFilterEndpoint endpoint = new StatusesFilterEndpoint();
@@ -36,7 +62,7 @@ public class App {
 		endpoint.followings(followings);
 		endpoint.trackTerms(terms);
 
-        Authentication hosebirdAuth = new OAuth1(
+		Authentication hosebirdAuth = new OAuth1(
         		Helper.properties().getProperty("consumerKey"),
         		Helper.properties().getProperty("consumerSecret"),
         		Helper.properties().getProperty("token"),
@@ -47,36 +73,18 @@ public class App {
 	        .hosts(hosebirdHosts)
 	        .authentication(hosebirdAuth)
 	        .endpoint(endpoint)
-	        .processor(new StringDelimitedProcessor(msgQueue))
-	        .eventMessageQueue(eventQueue);		// optional: use this if you want to process client events
+	        .processor(new StringDelimitedProcessor(msgQueue));
 
-		Client hosebirdClient = builder.build();
-		hosebirdClient.connect();
+		hosebirdClient = builder.build();
+	}
 
+	public static void setupKinesisClient() {
 		// Load AWS Credentials
 		BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(
 				Helper.properties().getProperty("awsSecretKey"), Helper.properties().getProperty("awsAccessKey"));
 
 		// Create the Amazon Kinesis Client
-		AmazonKinesisClient client = new AmazonKinesisClient(basicAWSCredentials);
-		client.setEndpoint("https://kinesis.us-east-1.amazonaws.com", "kinesis", "us-east-1");
-
-		while (!hosebirdClient.isDone()) {
-			String msg;
-			try {
-				msg = msgQueue.take();
-				System.out.println(msg);
-
-				// Add Data to a Stream
-				PutRecordRequest putRecordRequest = new PutRecordRequest();
-				putRecordRequest.setStreamName(Helper.properties().getProperty("kinesisStreamName"));
-				putRecordRequest.setData(ByteBuffer.wrap(String.format("testData-%s", msg).getBytes()));
-				putRecordRequest.setPartitionKey(String.format("partitionKey-%s", "tweets"));
-				client.putRecord(putRecordRequest);
-
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-    }
+		kinesisClient = new AmazonKinesisClient(basicAWSCredentials);
+		kinesisClient.setEndpoint("https://kinesis.us-east-1.amazonaws.com", "kinesis", "us-east-1");
+	}
 }
